959e36084399496132466b6e812b65b6090c6cd6
Beefalo-Cultist <119468340+Beefalo-Cultist@users.noreply.github.com>
2022-11-29 22:50:06 -0600

    Initial Upload
    Signed-off-by: Beefalo-Cultist <119468340+Beefalo-Cultist@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>(WORK IN PROGRESS) The Osmium Chalice</title>
+    <script src="OsJs.js"></script>
+    <style src="OsCss.css"></style>
+</head>
+
+<body>
+
+    <div id="welcome">
+        <h3>Hi!</h3>
+        <p>This will eventually be a chat site!</p>
+        <p>Alas, we are still under heavy development so please bear with us.</p>
+    </div>
+
+</body>
+
+</html>
