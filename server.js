59de81b245df80f05d9b16c011a69902d77b8869
Beefalo-Cultist <119468340+Beefalo-Cultist@users.noreply.github.com>
2022-12-02 19:09:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.static('public', { index: "mainpage.html", extensions: ['html', 'htm', 'png'] }))
 
+app.get('/mainreq', function (req, res) {
+    res.send('hi, S;KGLHKGc;iyflufLGD;iyfdLUY;Fltud;LUYFALutdflU.YDKTudKUYDKYG')
+})
+
 app.listen(8080, () => { console.log('connected!') })
