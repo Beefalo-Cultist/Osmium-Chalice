60e5bb9f2983ec7ca9f7db20708185393987e014
Beefalo-Cultist <119468340+Beefalo-Cultist@users.noreply.github.com>
2022-12-08 22:33:11 -0600

    Updated static js file serving

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public', { index: "mainpage.html", extensions: ['html', 'htm', 'png'] }))
+app.use(express.static('public', { index: "mainpage.html", extensions: ['html', 'htm', 'png'], }))
+app.use(express.static('public/js', { extensions: ['js'], setHeaders: setJsHeaders }))
+function setJsHeaders(res, path) {
+    res.type('js')
+}
 
 app.get('/mainreq', function (req, res) {
     res.send('hi, S;KGLHKGc;iyflufLGD;iyfdLUY;Fltud;LUYFALutdflU.YDKTudKUYDKYG')
 })
 
+
 app.listen(8080, () => { console.log('connected!') })
+
