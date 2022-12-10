c0883f65d1d6a7af0476230e5909ec2d73920fad
Beefalo-Cultist <119468340+Beefalo-Cultist@users.noreply.github.com>
2022-12-09 20:48:41 -0600

    minor updates

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ function setJsHeaders(res, path) {
     res.type('js');
 }
 
-app.get('/hi', function (req, res) {
+app.get('/hello', function (req, res) {
     res.send('<p>Message Received</p>');
     res.end();
 })
 
 app.all('/coffee', (req, res) => {
     console.warn("It is not advisable to attempt to brew coffee with a teapot")
-    res.status(418).send("The server responded with 418 I'm a teapot.<br>If you were not expecting the error, please make sure your next request is short and stout")
+    res.status(418).send("The server responded with 418 I'm a teapot.<br>If you were not expecting the error, please make sure your next request is short and stout.")
 })
 
 
