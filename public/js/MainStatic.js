17dbd9e17fdb6c8c5245e3c8ca7da4b1f23dcdbe
Beefalo-Cultist <119468340+Beefalo-Cultist@users.noreply.github.com>
2022-12-09 20:06:37 -0600

    Server update rollout and fetch reworks

--- public/js/MainStatic.js
+++ public/js/MainStatic.js
@@ -1,21 +1,5 @@
-function docload(id, url) {
-    ajaxreq = new XMLHttpRequest();
-    ajaxreq.onload = function () {
-        while (true) {
-            if (this.reponseText == undefined) {
-                continue
-            }
-            else {
-                document.getElementById(id).innerHTML = this.reponseText;
-                console.log("log: " + this.reponseTest)
-            }
-        }
-    }
-    ajaxreq.onreadystatechange = function () { console.log(this.readyState); console.log(this.status) };
-    ajaxreq.open("GET", url);
-    ajaxreq.send();
-}
-
-function hi() {
-    docload("welcome", "/mainreq")
+function fetchreq(url, options) {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => document.write(data));
 }
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
-app.use(express.static('public', { index: "mainpage.html", extensions: ['html', 'htm', 'png'], }))
-app.use(express.static('public/js', { extensions: ['js'], setHeaders: setJsHeaders }))
+app.use(express.static('public', { index: "mainpage.html", extensions: ['html', 'htm', 'png'], }));
+app.use(express.static('public/js', { extensions: ['js'], setHeaders: setJsHeaders }));
 function setJsHeaders(res, path) {
-    res.type('js')
+    res.type('js');
 }
 
-app.get('/mainreq', function (req, res) {
-    res.send('hi, S;KGLHKGc;iyflufLGD;iyfdLUY;Fltud;LUYFALutdflU.YDKTudKUYDKYG')
+app.get('/hi', function (req, res) {
+    res.send('<p>Message Received</p>');
+    res.end();
+})
+
+app.all('/coffee', (req, res) => {
+    console.warn("It is not advisable to attempt to brew coffee with a teapot")
+    res.status(418).send("The server responded with 418 I'm a teapot.<br>If you were not expecting the error, please make sure your next request is short and stout")
 })
 
 
-app.listen(8080, () => { console.log('connected!') })
+app.listen(8080, () => { console.log('connected!') });
 
