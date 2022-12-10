1a53f181ae7c8542f0844ae81952ab031e0516c6
Beefalo-Cultist <119468340+Beefalo-Cultist@users.noreply.github.com>
2022-12-09 21:07:05 -0600

    Misc. Updates

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public', { index: "mainpage.html", extensions: ['html', 'htm', 'png'], }));
 app.use(express.static('public/js', { extensions: ['js'], setHeaders: setJsHeaders }));
+const tea = "<head><title>Non TEA Compliant htcpcp Protocol Used</title><body>The server responded with 418 I'm a teapot.<br>If you were not expecting the error, please make sure your next request is short and stout.</body>"
 function setJsHeaders(res, path) {
     res.type('js');
 }
@@ -10,12 +11,12 @@ function setJsHeaders(res, path) {
 app.get('/hello', function (req, res) {
     res.send('<p>Message Received</p>');
     res.end();
-})
+});
 
 app.all('/coffee', (req, res) => {
     console.warn("It is not advisable to attempt to brew coffee with a teapot")
-    res.status(418).send("The server responded with 418 I'm a teapot.<br>If you were not expecting the error, please make sure your next request is short and stout.")
-})
+    res.status(418).send(tea)
+});
 
 
 app.listen(8080, () => { console.log('connected!') });
