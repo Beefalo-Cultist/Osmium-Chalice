a110956e6b65844d11b60b5ac9ed41c2d1bbfc0c
Beefalo-Cultist <beefalocultist@gmail.com>
2023-10-22 18:45:11 -0500

    Update MainStatic.js
    added getCookie() function and removed dbbt() function

--- public/js/MainStatic.js
+++ public/js/MainStatic.js
@@ -1,3 +1,10 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -12,17 +19,4 @@ function getCookie(cname) {
         }
     }
     return "";
-}
-
-function dbbt(url, id,) {
-    if (!getCookie('button')) {
-        document.cookie = "button=unpressed"
-    }
-
-    const options = {
-        credentials: 'include',
-    }
-    fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => { console.log(data); document.getElementById(id).innerHTML = data.text });
 }
